ef2f63dd64028dd775d5a63b48f96bd6bb6b563b
Guilherme Varandas <guil.varandas@shopify.com>
2020-10-03 00:03:54 -0400

    Update Config references to its new properties

--- src/components/explosion.js
+++ src/components/explosion.js
@@ -1,87 +1,96 @@
 import * as React from 'react';
-import { Animated, Dimensions, Easing } from 'react-native';
+import {Animated, Easing} from 'react-native';
 import BoxPiece from './boxpiece';
-import { Config } from '../config';
+import {Sizing} from '../config';
 
-class Explosion extends React.PureComponent {  
+class Explosion extends React.PureComponent {
   animation = new Animated.Value(0);
 
   constructor(props) {
-      super(props);
-      this.state = {
-        items: []
-      }
-    }
+    super(props);
+    this.state = {
+      items: [],
+    };
+  }
 
   componentDidMount = () => {
     this.calculateItems();
     setTimeout(() => {
-      this.animate()
+      this.animate();
     }, 50);
   };
 
   calculateItems = () => {
-    const { count, origin, backgroundColor } = this.props;
+    const {count, origin, backgroundColor} = this.props;
     const items = [];
     // Pixelize the box tile into small blocks that will be part of
     // explosion
     Array.from(Array(count).keys()).forEach(() => {
       const item = {
         backgroundColor: backgroundColor,
-        left: utils.randomValue(origin.x - 15, origin.x + Config.BOX_TILE_SIZE + 15),
-        top: utils.randomValue(origin.y - 15, origin.y + Config.BOX_TILE_SIZE + 15),
+        left: utils.randomValue(
+          origin.x - 15,
+          origin.x + Sizing.BOX_TILE_SIZE + 15
+        ),
+        top: utils.randomValue(
+          origin.y - 15,
+          origin.y + Sizing.BOX_TILE_SIZE + 15
+        ),
         width: utils.randomValue(2, 10),
-        height: utils.randomValue(2, 10)
+        height: utils.randomValue(2, 10),
       };
       items.push(item);
     });
 
     this.setState({
-      items
+      items,
     });
   };
 
   componentWillUnmount = () => {
-    if(this.exploAnimation) this.exploAnimation.stop();
-  }
+    if (this.exploAnimation) this.exploAnimation.stop();
+  };
 
   animate = () => {
-    const { fallSpeed = 300 } = this.props;   
+    const {fallSpeed = 300} = this.props;
     this.exploAnimation = Animated.timing(this.animation, {
       toValue: 1,
       duration: fallSpeed,
-      easing: Easing.linear
+      easing: Easing.linear,
     });
-    this.exploAnimation.start((() => {
+    this.exploAnimation.start(() => {
       // Clear items after explosion so that associated views are gc'd
       items = [];
-      this.setState({items})
-    }));
+      this.setState({items});
+    });
   };
 
   render() {
     return (
       <React.Fragment>
-        {this.state && this.state.items && this.state.items.map((item, index) => {
-          const bottom = this.animation.interpolate({
-            inputRange: [0, 1],
-            outputRange: [item.top, item.top + 100]
-          });
-          const opacity = this.animation.interpolate({
-            inputRange: [0, 1],
-            outputRange: [1, 0]
-          });       
-          return (
-            <BoxPiece 
-              backgroundColor={item.backgroundColor} 
-              left={item.left} 
-              height={item.height} 
-              width={item.width} 
-              top={bottom} 
-              opacity={opacity} 
-              key={index} />
-          );
-        })}
+        {this.state &&
+          this.state.items &&
+          this.state.items.map((item, index) => {
+            const bottom = this.animation.interpolate({
+              inputRange: [0, 1],
+              outputRange: [item.top, item.top + 100],
+            });
+            const opacity = this.animation.interpolate({
+              inputRange: [0, 1],
+              outputRange: [1, 0],
+            });
+            return (
+              <BoxPiece
+                backgroundColor={item.backgroundColor}
+                left={item.left}
+                height={item.height}
+                width={item.width}
+                top={bottom}
+                opacity={opacity}
+                key={index}
+              />
+            );
+          })}
       </React.Fragment>
     );
   }
